be83c8736d065728b0d4a59bad1f265c69a1e9ec
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-07-01 14:37:25 +0530

    Initial Commit

---
+++ Horoscope/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Horoscope
+//
+//  Created by Gokul Nair on 01/07/20.
+//  Copyright © 2020 Gokul Nair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
